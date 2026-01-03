38f4bfa9d1f274f089ef7d149d1751bfb9cf2b5e
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-01-03 14:48:17 -0500

    Update watch.php

--- videobird/watch.php
+++ videobird/watch.php
@@ -2,8 +2,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name=”robots” content=”noai, noimageai”>
-<title>VideoBird: Stream chicken footage for free!</title>
+<title>VideoBird</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -13,7 +12,10 @@
     <hr>
     <video controls playsinline disablepictureinpicture width=40%>
     <?php
-    echo '<source src="video/' . htmlspecialchars($_GET["video"]) . '" type="video/mp4"/>';
+    echo '<source src="video/' . htmlspecialchars($_GET["video"]) . '.mp4" type="video/mp4"/>';
+        if (file_exists("video/fallback/" . $_GET["video"] . ".webm")) {
+        echo '<source src="video/fallback/' . htmlspecialchars($_GET["video"]) . '.webm"' . '" type="video/webm; codecs=\'vp9, vorbis\'" />';
+    }
     ?>
 
 <p>
@@ -22,13 +24,17 @@
 </video>
 
 <?php
-echo '</br><a href="video/' . htmlspecialchars($_GET["video"]) . '" download>Download this video to play offline</a>';
-echo '</br><a href="video/' . htmlspecialchars($_GET["video"]) . '">View in fullscreen (may download MP4 in older browsers)</a></br></br>';
+/*include "addview.php";
+if ($updatedCount>=0){
+    echo '</br><i>This video has ' . $updatedCount . ' views.</i>';
+}
+*/
+echo '</br><a href="video/' . htmlspecialchars($_GET["video"]) . '.mp4" download>Download this video to play offline</a>';
 
 if (file_exists("video/sig/" . $_GET["video"] . ".sig")) {
     echo '<details><summary>This video is PGP-signed (click to inspect signatures)</summary>';
     echo '<p>This video has been signed by the owners of Mojavesoft.</br>The signature can be used to verify that your video download has not been tampered with.</p>';
-    echo '<a download href="' . 'video/sig/' . htmlspecialchars($_GET["video"]) . '.sig' . '">Download PGP signature for this video</a></br>';
+    echo '<a download href="' . 'video/sig/' . htmlspecialchars($_GET["video"]) . '.mp4.sig' . '">Download PGP signature for this video</a></br>';
     echo '<a download href="video/sig/pppdud.asc">Download PGP public key for Mojavesoft</a></br></details>';
 }
 
